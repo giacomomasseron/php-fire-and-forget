3c7e28bbcf88289cee353bb339ccf5aeb9bc2429
giacomomasseron <kylnas@tiscali.it>
2025-07-19 22:54:19 +0200

    feat: add put tests

---
+++ tests/Unit/PutTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use GiacomoMasseroni\FireAndForget\FireAndForget;
+
+it('slow api', function () {
+    $before = microtime(true);
+
+    FireAndForget::put('https://fakeresponder.com/?sleep=5000');
+    //exec("curl https://fakeresponder.com/?sleep=5000 > /dev/null 2>&1 &");
+
+    $after = microtime(true);
+
+    $this->assertTrue(($after - $before) < 1, 'The request should not block the execution');
+});
+
+it('slow api with bearer token', function () {
+    $before = microtime(true);
+
+    FireAndForget::withBearerToken('bearer_token')->put('https://fakeresponder.com/?sleep=5000');
+
+    $after = microtime(true);
+
+    $this->assertTrue(($after - $before) < 1, 'The request should not block the execution');
+});
+
+it('slow api with headers', function () {
+    $before = microtime(true);
+
+    FireAndForget::withHeaders([
+        'X-Header' => 'Test',
+        'X-Header2' => 'Test2',
+    ])->put('https://fakeresponder.com/?sleep=5000');
+
+    $after = microtime(true);
+
+    $this->assertTrue(($after - $before) < 1, 'The request should not block the execution');
+});
